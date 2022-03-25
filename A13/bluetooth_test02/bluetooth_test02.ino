e422476ef307d916a89c12fdc3aed4b4b3df4685
synthchris666 <63672369+lotkey@users.noreply.github.com>
2022-03-24 19:42:55 -0700

    fixed bluetooth code

--- A13/bluetooth_test02/bluetooth_test02.ino
+++ A13/bluetooth_test02/bluetooth_test02.ino
@@ -7,23 +7,32 @@
 
 BluetoothA2DPSink a2dp_sink;
 float averages[2];
+float maxes[2];
 float maximum = 0;
 
 void read_stream(const uint8_t* stream, uint32_t len) {
   int16_t *samples = (int16_t *)stream;
   int sample_count = len / 2;
   float sums[2] = {0, 0};
+  float maxbuffer[2] = {0, 0};
   
   for (int i = 0; i < sample_count; i++) {
+    if (abs(samples[i]) > maxbuffer[i % 2]) {
+      maxbuffer[i % 2] = samples[i];
+    }
     sums[i % 2] += abs(samples[i]);
   }
   
   if (sample_count == 0) {
     averages[0] = 0;
     averages[1] = 0;
+    maxes[0] = 0;
+    maxes[1] = 0;
   } else {
     averages[0] = sums[0] / ((float)sample_count / 2);
     averages[1] = sums[1] / ((float)sample_count / 2);
+    maxes[0] = maxbuffer[0];
+    maxes[1] = maxbuffer[1];
   }
 }
 
@@ -51,11 +60,21 @@ void setup() {
 
 void loop() {
   Board::Led::Pixels::clear();
-  Board::Util::Color color = Board::Util::Magenta;
-  float values[2] = {(averages[0] / 32767.0) * 255,
-                     (averages[1] / 32767.0) * 255};
-  Board::Led::Pixels::set({0, 1}, {values[0], 0, values[0]});
-  Board::Led::Pixels::set({2, 3}, {values[1], 0, values[1]});
+  float normalized[2] = {maxes[0] / 32767.0,
+                          maxes[1] / 32767.0};
+  int intensities[4][2];
+  intensities[0][0] = normalized[0] * 255;
+  intensities[0][1] = normalized[1] * 255;
+  intensities[1][0] = (normalized[0] - 0.25) / 0.25 * 255;
+  intensities[1][1] = (normalized[1] - 0.25) / 0.25 * 255;
+  intensities[2][0] = (normalized[0] - 0.5) / 0.25 * 255;
+  intensities[2][1] = (normalized[1] - 0.5) / 0.25 * 255;
+  intensities[3][0] = (normalized[0] - 0.75) / 0.25 * 255;
+  intensities[3][1] = (normalized[1] - 0.75) / 0.25 * 255;
+  Board::Led::Pixels::set(0, {0, intensities[0][0], intensities[0][1]});
+  Board::Led::Pixels::set(1, {0, intensities[1][0], intensities[1][1]});
+  Board::Led::Pixels::set(2, {0, intensities[2][0], intensities[2][1]});
+  Board::Led::Pixels::set(3, {0, intensities[3][0], intensities[3][1]});  
   Board::Led::Pixels::show();
-  delay(1);
+  // delay(1);
 }
